54b46c35cadd9b572aa2c13bd054f1909949f11a
houmanhaghi <rezahaghighatgou@gmail.com>
2023-07-03 10:49:07 +0330

    fix writing meta info in directories

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CC := gcc
 CFLAGS := -Wall -Wextra -g -pthread
 
 # Define the build target
-TARGET := FileSys
+TARGET := fisys
 
 # Define the directories you want to create
 ROOT := root
@@ -13,11 +13,12 @@ ROOT := root
 SUBDIRS := root/bin root/dev root/etc root/home root/mnt root/proc root/tmp root/usr
 
 # Define the default target (what happens when you just run `make` without any specific target)
-all: $(TARGET) create_dirs create_meta_files
+all: create_dirs create_meta_files $(TARGET)
 
 # Rule to build the C code
 $(TARGET): src/main.c
 	$(CC) $(CFLAGS) $< -o $@
+	
 
 # Rule to create directories (including subdirectories)
 create_dirs:
@@ -34,6 +35,10 @@ create_meta_files:
 	echo "" > $(ROOT)/proc/.meta
 	echo "" > $(ROOT)/tmp/.meta
 	echo "" > $(ROOT)/usr/.meta
+
+# need admin access to run binary file
+run:
+	sudo ./$(TARGET)
 
 # Rule to remove directories and the binary
 clean:
--- src/main.c
+++ src/main.c
@@ -1,39 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/statvfs.h>
+#include <unistd.h>
+
+struct statvfs vfs_stat;
+
+char all_dirs[256][256] = 
+    {"/root/bin/","/root/dev/","/root/etc/","/root/home/","/root/mnt/",
+    "/root/proc/","/root/tmp/","/root/usr/"};
 
 
-void create_sub_dir(char* sub_dir_name)
+void update_meta()
 {
-    char directory_path[100] = "./root/";
-    strcat(directory_path, sub_dir_name);
-    printf("%s\n", directory_path);
-    int result = mkdir(directory_path, 0766);
-    if (result == 0) {
-        printf("Directory created successfully.\n");
+    char path[256]; // PATH_MAX is a constant defined in <limits.h> that represents the maximum length of a path on the system.
+    if (getcwd(path, sizeof(path)) != NULL) {
     } else {
-        perror("Error creating directory\n");
+        perror("getcwd error");
         exit(1);
     }
+
+    int index = 0;
+    while(strcmp(all_dirs[index], "\0") != 0)
+    {
+        FILE *file;
+        char directory_path[256] = "";
+        strcat(directory_path, path);
+        strcat(directory_path, all_dirs[index]);
+        char  meta_path[256] = "";
+        strcat(meta_path ,directory_path);
+        strcat(meta_path, ".meta");
+
+
+        // Get the file system statistics using statvfs
+        if (statvfs(directory_path, &vfs_stat) == -1) {
+            perror("Error getting file system statistics");
+            exit(1);
+        }
+
+        file = fopen(meta_path, "w");
+        
+        // Check if the file was opened successfully
+        if (file == NULL) {
+        perror("Error opening the file");
+        exit(1);
+    }
+        // Print directory metadata
+        fprintf(file, "Directory: %s\n", directory_path);
+        fprintf(file, "Total size: %lu bytes\n", vfs_stat.f_frsize * vfs_stat.f_blocks);
+        fprintf(file, "Free space: %lu bytes\n", vfs_stat.f_frsize * vfs_stat.f_bfree );
+        fprintf(file, "Number of files: %lu\n", vfs_stat.f_files);
+
+        fclose(file);
+        index++;
+    }
+    
 }
+
 
 
 int main()
 {
-
-    // need admin access to create folders
-
-    // Implementing root directory and other sub-directories in here
-    const char* root_path = "./root";
-    int result = mkdir(root_path, 0766);
-    if (result == 0) {
-        printf("Root directory created successfully.\n");
-    } else {
-        perror("Error creating root directory\n");
-        exit(1);
-    }
-
-    create_sub_dir("bin");
+    update_meta();
 
 
 
